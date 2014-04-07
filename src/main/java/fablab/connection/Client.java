0bad0d047aeb93bcba44adf48ef17f85bcc3a07f
Nabil BENABBOU <nabil.benabbou@ensimag.grenoble-inp.fr>
2014-04-07 21:42:01 +0200

    cnx

--- src/main/java/fablab/connection/Client.java
+++ src/main/java/fablab/connection/Client.java
@@ -19,26 +19,33 @@ import java.util.logging.Logger;
  * @author nabilbenabbou1
  */
 public class Client {
-  
-    public static void main(String [] args) {
-        try {
-            String sentence;
-            BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-            while (true) {
-                sentence = inFromUser.readLine();
-                sendData(sentence);
-            }
-            
-        } catch (IOException ex) {
-            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+//  
+//    public static void main(String [] args) {
+//        try {
+//            String sentence;
+//            BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+//            while (true) {
+//                sentence = inFromUser.readLine();
+//                sendData(sentence);
+//            }
+//            
+//        } catch (IOException ex) {
+//            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
     
-    public static void sendData(String data) {
+    public synchronized String sendData(String data) {
         Socket clientSocket = null;
+        String respond = null;
         try {
             clientSocket = new Socket("localhost", 6789);
             DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            // If we need to wait for data from the server
+            if (data.startsWith("FIRST")) {
+                BufferedReader inFromServer =
+                        new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                respond = inFromServer.readLine();
+            }
             outToServer.writeBytes(data + '\n');
         } catch (Exception e) {
             e.printStackTrace();
@@ -49,5 +56,6 @@ public class Client {
                 Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+        return respond;
     }
 }
