59488a16520a4310fcf72eacd4e464ac17c84e2d
Thomas Cordier <thocordier@hotmail.fr>
2014-04-07 14:53:25 +0200

    ajout de qq interfaces, en cours de modif.

---
+++ src/main/java/fablab/Services/RemoteShell.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fablab.Services;
+
+/**
+ *
+ * @author cordieth
+ */
+public interface RemoteShell {
+    
+    /**
+     * Launch a shell (not implemented yet).
+     * 
+     */
+    
+    public void startShell();
+    
+}
